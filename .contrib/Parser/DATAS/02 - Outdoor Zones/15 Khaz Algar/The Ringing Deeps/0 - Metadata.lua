d9802da70c30e1769a105e6fd784b8da746a0489
ShadowBrooks <63622031+ShadowBrooks@users.noreply.github.com>
2024-10-26 17:37:10 -0700

    Fix mini-map error for coreway map missing

--- .contrib/Parser/DATAS/02 - Outdoor Zones/15 Khaz Algar/The Ringing Deeps/0 - Metadata.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/15 Khaz Algar/The Ringing Deeps/0 - Metadata.lua
@@ -5,5 +5,6 @@ root(ROOTS.Zones, m(KHAZ_ALGAR, bubbleDown({ ["timeline"] = { ADDED_11_0_2 } }, 
 	m(THE_RINGING_DEEPS, {
 		["icon"] = 5770812,
 		["lore"] = "This gigantic cavern serves as the home of the Machine Speakers, Earthen who maintain the gigantic Titan machines of old.",
+		["maps"] = { 2385 },	-- The Coreway (Ringing Deeps)
 	}),
 })));
