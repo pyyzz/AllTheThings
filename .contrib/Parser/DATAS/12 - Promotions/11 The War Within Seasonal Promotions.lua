610db46846788aa301f2082fae9a6ef2b7622187
Goldenshacal <goldenshacal@gmail.com>
2024-11-09 13:36:37 +0100

    added tiktok lul

--- .contrib/Parser/DATAS/12 - Promotions/11 The War Within Seasonal Promotions.lua
+++ .contrib/Parser/DATAS/12 - Promotions/11 The War Within Seasonal Promotions.lua
@@ -123,12 +123,12 @@ root(ROOTS.Promotions, {
 				["u"] = REAL_MONEY,
 			}),
 			i(223459, {	-- Blackrock Warsaber (MOUNT!)
-				["description"] = "Obtained through watching the official Warcraft 30th Anniversary Direct on Twitch or YouTube for 20 minutes between November 13th, 10:00 a.m. & December 11th, 10:00 a.m. PST.\n\nYour Twitch/YouTube account has to be connected with your Battle.net Account & you have to redeem the drop on Twitch before receiving it in your in-game collection as gift.",
+				["description"] = "Obtained through watching the official Warcraft 30th Anniversary Direct on Tiktok for 3 Minutes or Twitch/YouTube for 20 minutes between November 13th, 10:00 a.m. & December 11th, 10:00 a.m. PST.\n\nTikTok has a bar along the bottom of the screen that indicates that Game Rewards are live and that you’re earning progress. Once you watched enough on Tiktok, you will receive a code that can be claimed on Battle.Net or on the Battle.Net App.\n\nOn Twitch you have to claim your Reward under Drops & Rewards after watching for 20 minutes.\n\nOn YouTube your account has to say 'connected' and will automatically sent out the rewards after 20 minutes.\n\nYour Twitch/Youtube Account has to be connected with your Battle.net Account.",
 				["timeline"] = { ADDED_11_0_5, },
 				--["u"] = REMOVED_FROM_GAME,	-- 11th December 2024
 			}),
 			i(223471, {	-- Kaldorei War Wolf (MOUNT!)
-				["description"] = "Obtained through watching the official Warcraft 30th Anniversary Direct on Twitch or YouTube for 20 minutes between November 13th, 10:00 a.m. & December 11th, 10:00 a.m. PST.\n\nYour Twitch/YouTube account has to be connected with your Battle.net Account & you have to redeem the drop on Twitch before receiving it in your in-game collection as gift.",
+				["description"] = "Obtained through watching the official Warcraft 30th Anniversary Direct on Tiktok for 3 Minutes or Twitch/YouTube for 20 minutes between November 13th, 10:00 a.m. & December 11th, 10:00 a.m. PST.\n\nTikTok has a bar along the bottom of the screen that indicates that Game Rewards are live and that you’re earning progress. Once you watched enough on Tiktok, you will receive a code that can be claimed on Battle.Net or on the Battle.Net App.\n\nOn Twitch you have to claim your Reward under Drops & Rewards after watching for 20 minutes.\n\nOn YouTube your account has to say 'connected' and will automatically sent out the rewards after 20 minutes.\n\nYour Twitch/Youtube Account has to be connected with your Battle.net Account.",
 				["timeline"] = { ADDED_11_0_5, },
 				--["u"] = REMOVED_FROM_GAME,	-- 11th December 2024
 			}),
