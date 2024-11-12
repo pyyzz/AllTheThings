df8548edef285c8b47fcbc4ae108bb09828c324f
Darkal <michalbert@seznam.cz>
2024-11-12 22:19:17 +0100

    Added some of the TWW Hidden Currencies.

--- .contrib/Parser/DATAS/17 - NYI/Hidden Triggers/Hidden Currency Triggers.lua
+++ .contrib/Parser/DATAS/17 - NYI/Hidden Triggers/Hidden Currency Triggers.lua
@@ -6,6 +6,67 @@ root(ROOTS.HiddenCurrencyTriggers, {
 	expansion(EXPANSION.TWW, {
 		-- 11.0.2
 		expansion(EXPANSION.TWW, patch(0,2), bubbleDownSelf({ ["timeline"] = { ADDED_11_0_2 } }, {
+			currency(3005),	-- The General (Notoriety)
+			currency(3006),	-- The Vizier (Notoriety)
+			currency(3007),	-- The Weaver (Notoriety)
+			currency(3013),	-- Jewelcrafting Concentration
+			currency(3022),	-- Renown - Season 1 Delves
+			currency(3023),	-- 11.0 Professions - Personal Tracker - SI Spark Drops (Hidden)
+			currency(3024),	-- Cosmetic
+			currency(3025),	-- Cosmetic
+			currency(3026),	-- Cosmetic
+			currency(3027),	-- Cosmetic
+			currency(3040),	-- Blacksmithing Concentration
+			currency(3041),	-- Tailoring Concentration
+			currency(3042),	-- Leatherworking Concentration
+			currency(3043),	-- Inscription Concentration
+			currency(3044),	-- Engineering Concentration
+			currency(3045),	-- Alchemy Concentration
+			currency(3046),	-- Enchanting Concentration
+			currency(3047),	-- Jewelcrafting Concentration
+			currency(3048),	-- Tailoring Concentration
+			currency(3049),	-- Leatherworking Concentration
+			currency(3050),	-- Blacksmithing Concentration
+			currency(3051),	-- Enchanting Concentration
+			currency(3052),	-- Engineering Concentration
+			currency(3053),	-- Inscription Concentration
+			currency(3054),	-- Alchemy Concentration
+			currency(3057),	-- 11.0 Professions - Tracker - Weekly Alchemy Knowledge
+			currency(3058),	-- 11.0 Professions - Tracker - Weekly Blacksmithing Knowledge
+			currency(3059),	-- 11.0 Professions - Tracker - Weekly Enchanting Knowledge
+			currency(3060),	-- 11.0 Professions - Tracker - Weekly Engineering Knowledge
+			currency(3061),	-- 11.0 Professions - Tracker - Weekly Herbalism Knowledge
+			currency(3062),	-- 11.0 Professions - Tracker - Weekly Inscription Knowledge
+			currency(3063),	-- 11.0 Professions - Tracker - Weekly Jewelcrafting Knowledge
+			currency(3064),	-- 11.0 Professions - Tracker - Weekly Leatherworking Knowledge
+			currency(3065),	-- 11.0 Professions - Tracker - Weekly Mining Knowledge
+			currency(3066),	-- 11.0 Professions - Tracker - Weekly Skinning Knowledge
+			currency(3067),	-- 11.0 Professions - Tracker - Weekly Tailoring Knowledge
+			currency(3069),	-- 11.0 Professions - Tailoring - Fishing - Khaz Algar - Skill
+			currency(3070),	-- 11.0 Professions - Fishing - Algari Weaverthread - Perception
+			currency(3071),	-- 11.0 Professions - Fishing - Algari Weaverthread - Skill
+			currency(3072),	-- Everburning Ignition Refund
+			currency(3073),	-- 11.0 Professions - Tracker - Insc Book - Tailoring Knowledge
+			currency(3074),	-- 11.0 Professions - Tracker - Insc Book- Skinning Knowledge
+			currency(3075),	-- 11.0 Professions - Tracker - Insc Book - Mining Knowledge
+			currency(3076),	-- 11.0 Professions - Tracker - Insc Book - Leatherworking Know.
+			currency(3077),	-- 11.0 Professions - Tracker - Insc Book - Jewelcrafting Knowledge
+			currency(3078),	-- 11.0 Professions - Tracker - Insc Book - Inscription Knowledge
+			currency(3079),	-- 11.0 Professions - Tracker - Insc Book - Herbalism Knowledge
+			currency(3080),	-- 11.0 Professions - Tracker - Insc Book - Engineering Knowledge
+			currency(3081),	-- 11.0 Professions - Tracker - Insc Book - Enchanting Knowledge
+			currency(3082),	-- 11.0 Professions - Tracker - Insc Book - Blacksmithing Knowledge
+			currency(3083),	-- 11.0 Professions - Tracker - Insc Book - Alchemy Knowledge
+			currency(3084),	-- 11.0 Professions - Tracker - Insc Book - Inscription Knowledge
+			currency(3085),	-- 11.0 Delves - Personal Tracker - S1 Weekly Elise Turn-In(Hidden)
+			currency(3086),	-- DPS
+			currency(3087),	-- Tank
+			currency(3088),	-- Healer
+			currency(3099),	-- 11.0 Raid - Nerubian - Nerubar Finery Tracking Currency (Hidden)
+			currency(3102),	-- Bronze Celebration Token
+			currency(3103),	-- 11.0 Delves - System - Seasonal Affix - Events Active
+			currency(3104),	-- 11.0 Delves - System - Seasonal Affix - Events Maximum
+			currency(3115),	-- [DNT] Worldsoul Memory Score
 			currency(3142),	-- 11.0 Delves - Bountiful Tracker - Brann EXP Cap
 			currency(3143),	-- 11.0 Delves - Bountiful Tracker - Delver's Journey Cap
 		})),
