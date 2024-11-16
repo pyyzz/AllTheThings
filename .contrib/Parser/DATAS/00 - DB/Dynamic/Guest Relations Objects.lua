1d4cea8ff69f1e42cde5ac879450833bcce8407d
ShadowBrooks <63622031+ShadowBrooks@users.noreply.github.com>
2024-11-15 21:02:21 -0700

    Objectioes

--- .contrib/Parser/DATAS/00 - DB/Dynamic/Guest Relations Objects.lua
+++ .contrib/Parser/DATAS/00 - DB/Dynamic/Guest Relations Objects.lua
@@ -1,8 +1,258 @@
 ﻿local ObjectDB = ObjectDB; for objectID,objectData in pairs({
+	[456032] = {
+		readable = "Ruffled Paper",
+		model = 929401,
+		text = {
+			en = "Ruffled Paper",
+		},
+	},
+	[456036] = {
+		readable = "Loose Paper",
+		model = 936413,
+		text = {
+			en = "Loose Paper",
+		},
+	},
+	[456038] = {
+		readable = "Wet Paper",
+		model = 305393,
+		text = {
+			en = "Wet Paper",
+		},
+	},
+	[456039] = {
+		readable = "Crumpled Letter",
+		model = 1331238,
+		text = {
+			en = "Crumpled Letter",
+		},
+	},
+	[456041] = {
+		readable = "Folded Paper",
+		model = 311987,
+		text = {
+			en = "Folded Paper",
+		},
+	},
+	[456061] = {
+		readable = "Battered Celebration Crate",
+		model = 5282318,
+		text = {
+			en = "Battered Celebration Crate",
+		},
+	},
+	[462417] = {
+		readable = "Waterlogged Celebration Crate",
+		model = 5282318,
+		text = {
+			en = "Waterlogged Celebration Crate",
+		},
+	},
+	[463104] = {
+		readable = "Nola's Lash Wish",
+		model = 511490,
+		text = {
+			en = "Nola's Lash Wish",
+		},
+	},
+	[465234] = {
+		readable = "Missing Page from Yennu's Snackbook",
+		model = 936413,
+		text = {
+			en = "Missing Page from Yennu's Snackbook",
+		},
+	},
+	[465277] = {
+		readable = "Mound of Dirt",
+		model = 189528,
+		text = {
+			en = "Mound of Dirt",
+		},
+	},
 	[466438] = {
 		readable = "Charred Celebration Crate",
+		model = 5282318,
 		text = {
 			en = "Charred Celebration Crate",
+		},
+	},
+	[466512] = {
+		readable = "Crystallized Celebration Crate",
+		model = 877007,
+		text = {
+			en = "Crystallized Celebration Crate",
+		},
+	},
+	[467060] = {
+		readable = "Ball of Yarn",
+		model = 198143,
+		text = {
+			en = "Ball of Yarn",
+		},
+	},
+	[467075] = {
+		readable = "Darkmoon Fortune Card",
+		model = 5334402,
+		text = {
+			en = "Darkmoon Fortune Card",
+		},
+	},
+	[467083] = {
+		readable = "Darkmoon Fortune Card",
+		model = 5334402,
+		text = {
+			en = "Darkmoon Fortune Card",
+		},
+	},
+	[467293] = {
+		readable = "Chew Toy",
+		model = 1597754,
+		text = {
+			en = "Chew Toy",
+		},
+	},
+	[467305] = {
+		readable = "Toy Box",
+		model = 4252799,
+		text = {
+			en = "Toy Box",
+		},
+	},
+	[467329] = {
+		readable = "Maru's Toy",
+		model = 4615240,
+		text = {
+			en = "Maru's Toy",
+		},
+	},
+	[467334] = {
+		readable = "Darkmoon Fortune Card",
+		model = 5334402,
+		text = {
+			en = "Darkmoon Fortune Card",
+		},
+	},
+	[467336] = {
+		readable = "Darkmoon Fortune Card",
+		model = 5334402,
+		text = {
+			en = "Darkmoon Fortune Card",
+		},
+	},
+	[467338] = {
+		readable = "Darkmoon Fortune Card",
+		model = 5334402,
+		text = {
+			en = "Darkmoon Fortune Card",
+		},
+	},
+	[467339] = {
+		readable = "Darkmoon Fortune Card",
+		model = 5334402,
+		text = {
+			en = "Darkmoon Fortune Card",
+		},
+	},
+	[467342] = {
+		readable = "Darkmoon Fortune Card",
+		model = 5334402,
+		text = {
+			en = "Darkmoon Fortune Card",
+		},
+	},
+	[467351] = {
+		readable = "Darkmoon Fortune Card",
+		model = 5334402,
+		text = {
+			en = "Darkmoon Fortune Card",
+		},
+	},
+	[467403] = {
+		readable = "Darkmoon Fortune Card",
+		model = 5334402,
+		text = {
+			en = "Darkmoon Fortune Card",
+		},
+	},
+	[469985] = {
+		readable = "Ice Block",
+		model = 201047,
+		text = {
+			en = "Ice Block",
+		},
+	},
+	[470229] = {
+		readable = "Plush Toy",
+		model = 1021411,
+		text = {
+			en = "Plush Toy",
+		},
+	},
+	[472068] = {
+		readable = "Bakar Sketch",
+		model = 936413,
+		text = {
+			en = "Bakar Sketch",
+		},
+	},
+	[473934] = {
+		readable = "Property of Cooktholomew",
+		model = 200884,
+		text = {
+			en = "Property of Cooktholomew",
+		},
+	},
+	[475130] = {
+		readable = "Hidden Hatch",
+		model = 243849,
+		text = {
+			en = "Hidden Hatch",
+			es = "Trampilla oculta",
+			de = "Hidden Hatch",
+			fr = "Hidden Hatch",
+			it = "Hidden Hatch",
+			pt = "Hidden Hatch",
+			ru = "Hidden Hatch",
+			ko = "Hidden Hatch",
+			cn = "Hidden Hatch",
+		},
+	},
+	[478090] = {
+		readable = "Mildewed Celebration Crate",
+		model = 5282318,
+		text = {
+			en = "Mildewed Celebration Crate",
+		},
+	},
+	[505260] = {
+		readable = "Potion of Truth",
+		model = 892744,
+		text = {
+			en = "Potion of Truth",
+		},
+	},
+	[469568] = {
+		readable = "Burnt Feather",
+		text = {
+			en = "Burnt Feather",
+		},
+	},
+	[469569] = {
+		readable = "Plucked Feather",
+		text = {
+			en = "Plucked Feather",
+		},
+	},
+	[469573] = {
+		readable = "Frozen Feather",
+		text = {
+			en = "Frozen Feather",
+		},
+	},
+	[469574] = {
+		readable = "Normal Feather",
+		text = {
+			en = "Normal Feather",
 		},
 	},
 })
