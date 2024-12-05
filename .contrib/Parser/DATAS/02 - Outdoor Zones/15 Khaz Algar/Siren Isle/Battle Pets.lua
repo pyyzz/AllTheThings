3c8bb1f24559f588412650e082423a3c17d7164c
Alexandr Soft <37008350+AlexSoft1911@users.noreply.github.com>
2024-12-06 00:29:17 +0500

    PTR: Siren Isle battle pets

--- .contrib/Parser/DATAS/02 - Outdoor Zones/15 Khaz Algar/Siren Isle/Battle Pets.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/15 Khaz Algar/Siren Isle/Battle Pets.lua
@@ -1,23 +1,22 @@
 ---------------------------------------------------
 --          Z O N E S        M O D U L E         --
 ---------------------------------------------------
---[[
 root(ROOTS.Zones, m(KHAZ_ALGAR, bubbleDown({ ["timeline"] = { ADDED_11_0_7 } }, {
 	m(SIREN_ISLE, {
 		petbattle(filter(BATTLE_PETS, {
-			n(234734),	-- Battleboar Piglet (vrykul?)
-			n(234101),	-- Cave Kroling
-			n(234710),	-- Cliffreach Cub
-			n(),	-- Crackleroar (in-game journal: drop Stormtouched Pridetalon)
-			n(234367),	-- Pillaged Parrot (pirates/vrykul)
-			n(),	-- Proper Parrot (in-game journal: drop Pirates)
-			n(),	-- Rock Hound Mica (in-game journal: unique - probably some secret/special?)
-			n(234097),	-- Rusty Kroling
-			n(236041),	-- Scavenging Snapdragon (storm phase)
-			n(234369),	-- Snapdragon Pup
-			n(236040),	-- Storminfused Snapdragon (storm phase)
-			n(230394),	-- Tidal Kroling (pirates/vrykul)
-		}),
+			pet(4724),	-- Battleboar Piglet (vrykul?)
+			pet(4703),	-- Cave Kroling
+			pet(4723),	-- Cliffreach Cub
+			--pet(4726),	-- Crackleroar (in-game journal: drop Stormtouched Pridetalon) (itemID 234379?)
+			n(4710),	-- Pillaged Parrot (pirates/vrykul)
+			--pet(2541),	-- Proper Parrot (in-game journal: drop Pirates) (itemID 166358?)
+			--pet(4709),	-- Rock Hound Mica (in-game journal: unique - probably some secret/special?) (creatureID 234366/itemID 233057/spellID 473127, missing Siren Isle in wago tables)
+			pet(4702),	-- Rusty Kroling
+			pet(4732),	-- Scavenging Snapdragon (storm phase)
+			pet(4711),	-- Snapdragon Pup
+			pet(4731),	-- Storminfused Snapdragon (storm phase)
+			pet(4628),	-- Tidal Kroling (pirates/vrykul)
+			--pet(4725),	-- Titan Orb (wago - drop, missing description, spellID 474730, NYI?)
+		})),
 	}),
 })));
---]]
