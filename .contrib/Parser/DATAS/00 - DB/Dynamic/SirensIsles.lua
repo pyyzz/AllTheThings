b013d643c0a17b6828ad1c0d14bf1a52a478d8be
ShadowBrooks <63622031+ShadowBrooks@users.noreply.github.com>
2024-12-17 23:41:14 -0700

    Siren Objects

---
+++ .contrib/Parser/DATAS/00 - DB/Dynamic/SirensIsles.lua
@@ -0,0 +1,27 @@
+﻿local ObjectDB = ObjectDB; for objectID,objectData in pairs({
+	[454205] = {
+		readable = "Radiant Prism Crystals",
+		text = {
+			en = "Radiant Prism Crystals",
+		},
+	},
+	[456869] = {
+		readable = "Kaja'Cola Stash",
+		text = {
+			en = "Kaja'Cola Stash",
+		},
+	},
+	[500697] = {
+		readable = "Well Loved Squeaky Toy",
+		text = {
+			en = "Well Loved Squeaky Toy",
+		},
+	},
+	[505476] = {
+		readable = "Stone Carver's Scramseax",
+		text = {
+			en = "Stone Carver's Scramseax",
+		},
+	},
+})
+do ObjectDB[objectID] = objectData; end
