70d1eaecf4dc9e60274df1a59fc3853051210ada
Dmitry Volodin <mr.molkree@gmail.com>
2024-12-18 17:35:57 +0400

    Mark new Diablo promo as time limited

--- .contrib/Parser/DATAS/12 - Promotions/Diablo Events.lua
+++ .contrib/Parser/DATAS/12 - Promotions/Diablo Events.lua
@@ -141,12 +141,12 @@ root(ROOTS.Promotions, n(DIABLO_EVENTS, {
 	}))),
 	i(206007, {	-- Treasure Nabbin' Bag
 		["description"] = "Can be earned by logging into Diablo Immortal on a level 10+ character during the 'Eternal War' crossover event between November 13, 3:00 a.m. & December 11, 2:59 a.m. local server time.",
-		["timeline"] = { ADDED_11_0_5 },
+		["timeline"] = { "added 11.0.5.57388", "removed 11.0.5.57689" },
 		["u"] = REMOVED_FROM_GAME,
 	}),
 	n(DIABLO_IV, {
-		["description"] = "Granted to players who owned Diablo IV: Vessel of Hatred before January 7th, 2025.",
-		["timeline"] = { ADDED_11_0_7 },
+		["description"] = "Granted to players who owned WoW: The War Within and Diablo IV: Vessel of Hatred before January 7th, 2025.",
+		["timeline"] = { ADDED_11_0_7, "removed 11.0.7.99999" },
 		["g"] = {
 			i(191114),	-- Amalgam of Rage (MOUNT!)
 			i(206018),	-- Baa'lial (PET!)
