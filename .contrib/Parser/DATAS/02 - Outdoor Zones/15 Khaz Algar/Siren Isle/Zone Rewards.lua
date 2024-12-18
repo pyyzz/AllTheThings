3ab49071ace1949c2f3693fba82a020746d60c2f
ShadowBrooks <63622031+ShadowBrooks@users.noreply.github.com>
2024-12-17 22:51:12 -0700

    Update Zone Rewards.lua

--- .contrib/Parser/DATAS/02 - Outdoor Zones/15 Khaz Algar/Siren Isle/Zone Rewards.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/15 Khaz Algar/Siren Isle/Zone Rewards.lua
@@ -22,7 +22,7 @@ root(ROOTS.Zones, m(KHAZ_ALGAR, bubbleDown({ ["timeline"] = { ADDED_11_0_7 } }, 
 				i(233827),	-- Bilge Rat Meat Tenderizer (COSMETIC!)
 			}),
 			n(228099, {	-- Blinky Greasefingers
-				["description"] = "Click on dialog option that pop, after you learn Snapdragom mount",
+				["description"] = "Click on dialog option that pop, after you learn Snapdragon mount",
 				--["lockCriteria"] = {1, "questID", 86485 },	-- TODO: probably a spellID will be better?
 				["coord"] = { 69.2, 45.8, SIREN_ISLE },
 				["g"] = {
