73d1b8ec0b5b848d22798714af9719cbadfa9bbb
Chun-Chi Hung <b10102229@gmail.com>
2024-12-24 11:12:36 +0800

    [Localization] Update zhTW:Demon Traniner.

--- .contrib/Parser/lib/Headers/Common/Demon Trainer.lua
+++ .contrib/Parser/lib/Headers/Common/Demon Trainer.lua
@@ -10,8 +10,10 @@ DEMON_TRAINER = createHeader({
 		ru = "Наставник демонов",
 		ko = "악마 훈련사",
 		cn = "恶魔训练师",
+		tw = "惡魔訓練師",
 	},
 	description = {
 		en = "Warlocks can teach their demons new tricks. Some of the higher level grimoires can only be purchased from the Demon Trainer in your faction's capital cities.",
+		tw = "術士可以教導他們的惡魔新技能，一些高等級的魔典只能從你陣營首都的惡魔訓練師那裡購買。",
 	},
 });
