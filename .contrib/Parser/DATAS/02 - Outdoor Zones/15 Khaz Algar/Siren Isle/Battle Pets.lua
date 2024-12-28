6beccc1c7e31e5b95bd63773b41d28cc053dc621
ShadowBrooks <63622031+ShadowBrooks@users.noreply.github.com>
2024-12-27 21:27:21 -0800

    Update Battle Pets.lua
    Snapdragon Pup only available during Naga Invasions.

--- .contrib/Parser/DATAS/02 - Outdoor Zones/15 Khaz Algar/Siren Isle/Battle Pets.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/15 Khaz Algar/Siren Isle/Battle Pets.lua
@@ -11,7 +11,9 @@ root(ROOTS.Zones, m(KHAZ_ALGAR, bubbleDown({ ["timeline"] = { ADDED_11_0_7 } }, 
 			--pet(2541),	-- Proper Parrot (in-game journal: drop Pirates) (itemID 166358?)
 			--pet(4709),	-- Rock Hound Mica (in-game journal: unique - probably some secret/special?) (creatureID 234366/itemID 233057/spellID 473127, missing Siren Isle in wago tables)
 			pet(4702),	-- Rusty Kroling
-			pet(4711),	-- Snapdragon Pup
+			pet(4711, {	-- Snapdragon Pup
+				["description"] = "Only spawns during Naga invasion week.",
+			}),
 			pet(4628),	-- Tidal Kroling (pirates/vrykul)
 			--pet(4725),	-- Titan Orb (wago - drop, missing description, spellID 474730, NYI?)
 		})),
