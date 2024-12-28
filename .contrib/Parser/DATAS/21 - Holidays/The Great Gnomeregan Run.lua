3936fc61a7f767b8bc48b669304cb8eff926539a
Chun-Chi Hung <b10102229@gmail.com>
2024-12-28 23:48:43 +0800

    [Localization] Update zhTW: The Great Gnomeregan Run.

--- .contrib/Parser/DATAS/21 - Holidays/The Great Gnomeregan Run.lua
+++ .contrib/Parser/DATAS/21 - Holidays/The Great Gnomeregan Run.lua
@@ -10,6 +10,7 @@ THE_GREAT_GNOMEREGAN_RUN_HEADER = createHeader({
 		en = "The Great Gnomeregan Run",
 		ru = "Великая гонка Гномрегана",
 		cn = "诺莫瑞根马拉松",
+		tw = "偉大的諾姆瑞根路跑",
 	},
 });
 root(ROOTS.Holidays, applyevent(EVENTS.THE_GREAT_GNOMEREGAN_RUN, n(THE_GREAT_GNOMEREGAN_RUN_HEADER, bubbleDownSelf({ ["timeline"] = { ADDED_7_2_5 } }, {
