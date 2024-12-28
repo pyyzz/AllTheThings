30b8dead565169ed28b7945d4510a21f3729155e
Chun-Chi Hung <b10102229@gmail.com>
2024-12-29 01:15:04 +0800

    [Localization] Update zhTW: The Scepter of the Shifting Sands.

--- .contrib/Parser/DATAS/07 - Events/The Scepter of the Shifting Sands/The Scepter of the Shifting Sands.lua
+++ .contrib/Parser/DATAS/07 - Events/The Scepter of the Shifting Sands/The Scepter of the Shifting Sands.lua
@@ -14,6 +14,7 @@ THE_SCEPTER_OF_THE_SHIFTING_SANDS = createHeader({
 		ru = "Скипетр Зыбучих песков",
 		ko = "흐르는 모래의 홀",
 		cn = "流沙节杖",
+		tw = "流沙節杖",
 	},
 	description = {
 		en = "In addition to all players on a realm working towards completing the War Effort, one player had to create the Scepter of the Shifting Sands - a very difficult task that could only be solved by a server's top guild back when the questline was added. After the war effort was finished and the army of the Alliance and the Horde arrived in Silithus, the Scepter of the Shifting Sands could be used on the Scarab Gong. Doing so resulted in the opening of Ahn'Qiraj. The first player to bang the Scarab Gong on each server would be rewarded with the Scarab Lord title (in Burning Crusade, the title will not be available in Classic!) and the Black Qiraji Resonating Crystal mount. Anyone else who followed them within 10 hours was rewarded with the title as well.\n\nAlthough it is still possible to complete the questline for the Scepter of the Shifting Sands after the gates have been opened on your server, doing so will not reward neither the Black Qiraji Resonating Crystal nor the Scarab Lord title.",
