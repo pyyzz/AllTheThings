5b1b493d83c2a8bc3c6b2a84bd578e96257937c8
Chun-Chi Hung <b10102229@gmail.com>
2024-12-29 11:15:45 +0800

    [DB] Add event schedule: Remix:MoP.

--- .contrib/Parser/lib/Headers/Remix/Mists of Pandaria.lua
+++ .contrib/Parser/lib/Headers/Remix/Mists of Pandaria.lua
@@ -4,6 +4,11 @@ REMIX_MOP = createHeader({
 	minilist_ignore = true,
 	icon = [[~_.asset("Category_Remix")]],
 	eventID = EVENTS.REMIX_MOP,
+	eventSchedule = {
+		0, -- May 16th, 2024 to August 20th, 204
+		2024, 5, 16,	-- 5/16/2024
+		2024, 8, 20,	-- 8/20/2024
+	},
 	timerunningSeasonID = 1,
 	text = {
 		en = "Remix: Mists of Pandaria",
